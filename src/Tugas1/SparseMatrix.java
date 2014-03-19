fae70bcdee1a920c699d0664609baf292722061e
hanifnaufal <hanifnaufal7557@gmail.com>
2014-03-19 20:12:05 +0700

    Dokumentasi program

--- src/Tugas1/SparseMatrix.java
+++ src/Tugas1/SparseMatrix.java
@@ -12,6 +12,7 @@ import java.util.List;
  * @author Anum A07
  */
 public class SparseMatrix {
+
     public int rowSize;
     public int colSize;
     public int[] P;
@@ -20,47 +21,73 @@ public class SparseMatrix {
 
     /**
      * fungsi untuk membuat Sparse Matrix berisi nilai nol
+     *
      * @param size panjang baris dan kolom matriks
      * @return matriks berisi nilai nol
      */
-    public static SparseMatrix zeros(int size){
+    public static SparseMatrix zeros(int size) {
         return new SparseMatrix(size);
     }
 
+    /**
+     * Konstruktor Sparse Matrix
+     *
+     * @param A matriks dense
+     */
     public SparseMatrix(double[][] A) {
         this.colSize = A[0].length;
-		this.rowSize = A.length;
-        P = new int[colSize+1];
+        this.rowSize = A.length;
+        P = new int[colSize + 1];
         I = new ArrayList<Integer>();
         X = new ArrayList<Double>();
         createMatrix(A);
     }
 
-    public SparseMatrix(double[] b){
-        this(b.length,1);
-        for (int i=0; i<b.length; i++){
-            setElement(i,0,b[i]);
+    /**
+     * Konstruktor Sparse Matrix
+     *
+     * @param b matriks dense
+     */
+    public SparseMatrix(double[] b) {
+        this(b.length, 1);
+        for (int i = 0; i < b.length; i++) {
+            setElement(i, 0, b[i]);
         }
     }
-    
+
+    /**
+     * Konstruktor Sparse Matrix Berfungsi untuk membuat Sparse Matrix size x
+     * size
+     *
+     * @param size ukuran matrix
+     */
     public SparseMatrix(int size) {
         this.rowSize = this.colSize = size;
-        P = new int[colSize+1];
+        P = new int[colSize + 1];
         I = new ArrayList<Integer>();
         X = new ArrayList<Double>();
         //hardcodeYeay();
     }
 
-    public SparseMatrix(int rowSize, int colSize){
-        this.rowSize = rowSize; this.colSize = colSize;
-        P = new int[colSize+1];
+    /**
+     * Konstruktor Sparse Matrix Berfungsi untuk membuat Sparse Matrix ukuran
+     * rowSize x colSize
+     *
+     * @param rowSize ukuran baris pada matrix
+     * @param colSize ukuran kolom pada matrix
+     */
+    public SparseMatrix(int rowSize, int colSize) {
+        this.rowSize = rowSize;
+        this.colSize = colSize;
+        P = new int[colSize + 1];
         I = new ArrayList<Integer>();
         X = new ArrayList<Double>();
     }
-    
+
     /**
-     * Membuat representasi sparse matriks dari matriks double[][]
-     * @param A representasi double[][]
+     * Membuat representasi sparse matriks dari matriks dense double[][]
+     *
+     * @param A matrix dense double[][]
      */
     private void createMatrix(double[][] A) {
         int numItemAdded = 0;
@@ -74,11 +101,18 @@ public class SparseMatrix {
                 }
             }
         }
-        P[P.length-1] = numItemAdded;
+        P[P.length - 1] = numItemAdded;
     }
-    
+
+    /**
+     * Mengambil elemen
+     *
+     * @param row index baris
+     * @param col index kolom
+     * @return nilai
+     */
     public double getElement(int row, int col) {
-        for (int i = P[col]; i < P[col+1]; i++) {
+        for (int i = P[col]; i < P[col + 1]; i++) {
             if (I.get(i) == row) {
                 return X.get(i);
             }
@@ -86,22 +120,33 @@ public class SparseMatrix {
         return 0.0;
     }
 
-
-    public SparseMatrix getColumn(int col){
-        SparseMatrix result = new SparseMatrix(this.rowSize,1);
-        result.P[1] = this.P[col+1]-this.P[col];
-        result.I = this.I.subList(this.P[col],this.P[col+1]);
-        result.X = this.X.subList(this.P[col],this.P[col+1]);
+    /**
+     * Mengambil matrix kolom
+     *
+     * @param col index kolom
+     * @return matrix kolom ke col
+     */
+    public SparseMatrix getColumn(int col) {
+        SparseMatrix result = new SparseMatrix(this.rowSize, 1);
+        result.P[1] = this.P[col + 1] - this.P[col];
+        result.I = this.I.subList(this.P[col], this.P[col + 1]);
+        result.X = this.X.subList(this.P[col], this.P[col + 1]);
         return result;
     }
 
-    
+    /**
+     * Mencari nilai absolut terbesar dari satu kolom
+     *
+     * @param rowStart baris dimulainya pencarian
+     * @param invpM invers dari matrix permutasi
+     * @return index baris dimana nilai terbesar berada
+     */
     public int searchMaxRowOneColumn(int rowStart, int[] invpM) {
         double max = Double.MIN_VALUE;
         int maxRow = -1;
 
         for (int i = P[0]; i < P[1]; i++) {
-            if(invpM[I.get(i)] >= rowStart){
+            if (invpM[I.get(i)] >= rowStart) {
                 double val = Math.abs(X.get(i));
                 if (val > max) {
                     max = val;
@@ -111,66 +156,90 @@ public class SparseMatrix {
         }
         return (maxRow != -1) ? invpM[maxRow] : -1;
     }
-	
-	public SparseMatrix permute(int[] pM) {
-		int[] invpM = new int[pM.length];
-		
-		for (int i = 0; i < pM.length; i++)
-			invpM[pM[i]] = i; //invpM => index = value, value = index
-		
-		for (int i = 0; i < colSize; i++) { // for each column
-			for (int j = P[i]; j < P[i+1]; j++) { //for each available row
-				I.set(j, invpM[I.get(j)]); //I[row] = inv[I[row]]
-			}
-		}
-        return this;
-	}
+    
+    /**
+     * Fungsi yang digunakan untuk melakukan permutasi baris 
+     * @param pM matrix permutasi
+     * @return Sparse Matrix yang telah dipermutasi 
+     */
+    public SparseMatrix permute(int[] pM) {
+        int[] invpM = new int[pM.length];
 
+        for (int i = 0; i < pM.length; i++) {
+            invpM[pM[i]] = i; //invpM => index = value, value = index
+        }
+        for (int i = 0; i < colSize; i++) { // for each column
+            for (int j = P[i]; j < P[i + 1]; j++) { //for each available row
+                I.set(j, invpM[I.get(j)]); //I[row] = inv[I[row]]
+            }
+        }
+        return this;
+    }
+
+    /**
+     * Fungsi yang digunakan untuk menghapus elemen 
+     * @param row baris 
+     * @param col kolom
+     */
     public void removeElement(int row, int col) {
-        for (int i = P[col]; i < P[col+1]; i++) {
+        for (int i = P[col]; i < P[col + 1]; i++) {
             if (I.get(i) == row) {
                 X.remove(i);
                 I.remove(i);
-                for (int j = col+1; j < P.length; j++) {
+                for (int j = col + 1; j < P.length; j++) {
                     P[j]--;
                 }
             }
         }
     }
 
+    /**
+     * Fungsi yang digunakan untuk mengubah nilai elemen 
+     * @param row baris 
+     * @param col kolom
+     * @param val nilai baru
+     */
     public void setElement(int row, int col, double val) {
-        if(Math.abs(val) < 1e-10){
-            removeElement(row,col);
+        if (Math.abs(val) < 1e-10) {
+            removeElement(row, col);
             return;
         }
-        for (int i = P[col]; i < P[col+1]; i++) {
+        for (int i = P[col]; i < P[col + 1]; i++) {
             if (I.get(i) == row) {
                 X.set(i, val);
-				return;
+                return;
             }
         }
-        I.add(P[col+1], row);
-        X.add(P[col+1], val);
-        for(int j = col+1; j< P.length; j++) P[j]++;
+        I.add(P[col + 1], row);
+        X.add(P[col + 1], val);
+        for (int j = col + 1; j < P.length; j++) {
+            P[j]++;
+        }
     }
-    
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < rowSize; i++) {
             for (int j = 0; j < colSize; j++) {
-                sb.append(String.format("%10.5f",getElement(i,j))).append(";");
+                sb.append(String.format("%10.5f", getElement(i, j))).append(";");
             }
             sb.append("\n");
         }
         return sb.toString();
     }
 
-    public double[][] toDenseMatrix(){
+    /**
+     * Melakukan konversi Sparse matrix menjadi dense matrix
+     * @return dense matrix hasil konversi 
+     */
+    public double[][] toDenseMatrix() {
         double[][] result = new double[rowSize][colSize];
-        for (int i=0; i<result.length; i++)
-            for(int j=0; j<result[i].length; j++)
-                result[i][j] = getElement(i,j);
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                result[i][j] = getElement(i, j);
+            }
+        }
         return result;
     }
 }
--- src/Tugas1/SparseSolver.java
+++ src/Tugas1/SparseSolver.java
@@ -12,128 +12,173 @@ import java.util.List;
  * @author Anum A07
  */
 public class SparseSolver {
+
     SparseMatrix U;
     SparseMatrix L;
     SparseMatrix A;
     SparseMatrix b;
-	int[] pM;
+    int[] pM;
     int[] invpM;
-    
+
+    /**
+     * Inisialisasi SparseSolver dengan matriks A dan b
+     *
+     * @param A matriks dense dua dimensi
+     * @param b matriks dense satu dimensi
+     */
     public void init(double[][] A, double[] b) {
         this.A = new SparseMatrix(A);
         this.U = SparseMatrix.zeros(A.length);
         this.L = SparseMatrix.zeros(A.length);
         this.b = new SparseMatrix(b);
-		pM = new int[A.length];
-		invpM = new int[A.length];
+        pM = new int[A.length];
+        invpM = new int[A.length];
         for (int i = 0; i < A.length; i++) {
-			pM[i] = i;
+            pM[i] = i;
             invpM[pM[i]] = i;
-		}
+        }
     }
-    
-    public double[] hitungLU(double[][] A, double[] b){
+
+    /**
+     * Method ini digunakan untuk mencari solusi sistem persamaan linier
+     *
+     * @param A matriks dense dua dimensi
+     * @param b matriks dense satu dimensi
+     * @return solusi sistem persamaan linier
+     */
+    public double[] hitungLU(double[][] A, double[] b) {
         init(A, b);
         leftLooking();
         SparseMatrix y = forwardElimination(L, this.b);
-        double[] x = backwardSubstitution(U,y);
+        double[] x = backwardSubstitution(U, y);
         // L.permute(pM); //membenarkan L untuk dilihat
         return x;
     }
 
-	public void leftLooking() {
+    /**
+     * Dekomposisi matrix A menjadi matrix L dan U
+     */
+    public void leftLooking() {
         int lp = 0, up = 0; // index buat P pada L dan U
-		for (int j = 0; j < A.colSize; j++) {
+        for (int j = 0; j < A.colSize; j++) {
 
-			//get j-th column of A
-			SparseMatrix Aij = A.getColumn(j);
+            //get j-th column of A
+            SparseMatrix Aij = A.getColumn(j);
 
-			//solve Ly = Aj
-			SparseMatrix y = forwardElimination(L, Aij);
-			
-			//pivoting y
-			int maxRow = y.searchMaxRowOneColumn(j,invpM);
-			if (maxRow != j && maxRow > -1) {
-				pM[j] ^= pM[maxRow];
-				pM[maxRow] ^= pM[j];
-				pM[j] ^= pM[maxRow];
+            //solve Ly = Aj
+            SparseMatrix y = forwardElimination(L, Aij);
+
+            //pivoting y
+            int maxRow = y.searchMaxRowOneColumn(j, invpM);
+            if (maxRow != j && maxRow > -1) {
+                pM[j] ^= pM[maxRow];
+                pM[maxRow] ^= pM[j];
+                pM[j] ^= pM[maxRow];
                 // hitung inverse p
-                for (int i = 0; i < pM.length; i++)
+                for (int i = 0; i < pM.length; i++) {
                     invpM[pM[i]] = i; //invpM => index = value, value = index
+                }
             }
 
-
-			//insert to matrix UL
+            //insert to matrix UL
             lp++;
             L.I.add(pM[j]);
             L.X.add(1.0);
-            double pivotElem = y.getElement(pM[j],0);
+            double pivotElem = y.getElement(pM[j], 0);
 
-            for (int i = y.P[0]; i<y.P[1]; i++){
-                if(invpM[y.I.get(i)] <= j){
+            for (int i = y.P[0]; i < y.P[1]; i++) {
+                if (invpM[y.I.get(i)] <= j) {
                     U.I.add(invpM[y.I.get(i)]);
                     U.X.add(y.X.get(i));
                     up++;
-                }
-                else{
+                } else {
                     L.I.add(y.I.get(i));
-                    L.X.add(y.X.get(i)/pivotElem);
+                    L.X.add(y.X.get(i) / pivotElem);
                     lp++;
                 }
             }
-            U.P[j+1] = up;
-            L.P[j+1] = lp;
-		}
-	}
-	
+            U.P[j + 1] = up;
+            L.P[j + 1] = lp;
+        }
+    }
+
+    /**
+     * Mencari solusi persamaan linier matriks segitiga bawah L dan matriks b 
+     * dengan menggunakan metode fowardElimination
+     * @param L matriks segitiga bawah
+     * @param b matriks kolom
+     * @return solusi sistem persamaan linier
+     */
     public SparseMatrix forwardElimination(SparseMatrix L, SparseMatrix b) {
         // x = b
         double[] x = new double[b.rowSize];
-        for(int i = b.P[0]; i< b.P[1]; i++){
+        for (int i = b.P[0]; i < b.P[1]; i++) {
             x[b.I.get(i)] = b.X.get(i);
         }
 
-        List<Integer> Xr = reach(L,b);
-        for(int k = Xr.size()-1; k>=0; k--){
+        List<Integer> Xr = reach(L, b);
+        for (int k = Xr.size() - 1; k >= 0; k--) {
             int j = Xr.get(k);
-            for(int i=L.P[j]; i<L.P[j+1]; i++) if(invpM[L.I.get(i)] > j){
-                x[L.I.get(i)] = x[L.I.get(i)] - L.X.get(i)*x[pM[j]];
+            for (int i = L.P[j]; i < L.P[j + 1]; i++) {
+                if (invpM[L.I.get(i)] > j) {
+                    x[L.I.get(i)] = x[L.I.get(i)] - L.X.get(i) * x[pM[j]];
+                }
             }
         }
         return new SparseMatrix(x);
     }
-
-    public List<Integer> reach(SparseMatrix L, SparseMatrix b){
+    
+    /**     
+     * fungsi untuk mendapatkan index baris - baris yang dicapai dari setiap 
+     * index pada Sparse Matrix b 
+     * @param L matrix segitiga bawah
+     * @param b matrix kolom
+     * @return index baris - baris x yang tidak sama dengan 0
+     */
+    public List<Integer> reach(SparseMatrix L, SparseMatrix b) {
         boolean[] mark = new boolean[L.colSize];
         ArrayList<Integer> Xr = new ArrayList<Integer>();
-        for(int i : b.I){
-            if(!mark[invpM[i]]){
-                dfs(invpM[i],L,mark,Xr);
+        for (int i : b.I) {
+            if (!mark[invpM[i]]) {
+                dfs(invpM[i], L, mark, Xr);
             }
         }
         return Xr;
     }
-
-    public void dfs(int j, SparseMatrix L, boolean[] mark, List<Integer> Xr){
+    
+    /**
+     * Fungsi traversal search
+     * @param j posisi vertex yang sedang dikunjungi
+     * @param L matrix segitiga bawah 
+     * @param mark penandaan vertex yang sudah dikunjungi
+     * @param Xr stack yang berisi index - index yang tidak sama dengan 0
+     */
+    public void dfs(int j, SparseMatrix L, boolean[] mark, List<Integer> Xr) {
         mark[j] = true;
-        for(int i = L.P[j]; i<L.P[j+1]; i++){
-            if(!mark[invpM[L.I.get(i)]]){
-                dfs(invpM[L.I.get(i)],L,mark,Xr);
+        for (int i = L.P[j]; i < L.P[j + 1]; i++) {
+            if (!mark[invpM[L.I.get(i)]]) {
+                dfs(invpM[L.I.get(i)], L, mark, Xr);
             }
         }
         Xr.add(j);
     }
 
-
+    /**
+     * Mencari solusi persamaan linier matriks segitiga atas U dan matriks y 
+     * dengan menggunakan metode backwardSubstitution
+     * @param U matriks segitiga atas
+     * @param y matriks kolom
+     * @return 
+     */
     public double[] backwardSubstitution(SparseMatrix U, SparseMatrix y) {
         double[] x = new double[y.rowSize];
-        for(int i=0; i<y.P[1]; i++){
+        for (int i = 0; i < y.P[1]; i++) {
             x[invpM[y.I.get(i)]] = y.X.get(i);
         }
-        for(int i=U.colSize-1; i >= 0; i--){
-            x[i]= x[i] / U.getElement(i,i);
-            for(int j=i-1; j >= 0; j--){
-                x[j]=x[j] - U.getElement(j,i)*x[i];
+        for (int i = U.colSize - 1; i >= 0; i--) {
+            x[i] = x[i] / U.getElement(i, i);
+            for (int j = i - 1; j >= 0; j--) {
+                x[j] = x[j] - U.getElement(j, i) * x[i];
             }
         }
         return x;
